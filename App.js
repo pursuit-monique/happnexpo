bd03b09954e26f0109de818061d3da9097f177ba
Monique <107360758+pursuit-monique@users.noreply.github.com>
2023-10-07 19:17:59 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, ScrollView, StyleSheet, Image } from "react-native";
+import { Text, Card, Button, Icon, Input } from "@rneui/themed";
 
-export default function App() {
+const CardsComponentsProps = {};
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ScrollView>
+        <View style={styles.container}>
+          <Card style={{ marginTop: 30 }}>
+            <Card.Title>Sign In</Card.Title>
+            <Card.Divider />
+            <Input
+              placeholder="Username"
+              leftIcon={{ type: "font-awesome", name: "user" }}
+            />
+            <Input
+              placeholder="Email"
+              leftIcon={{ type: "font-awesome", name: "envelope" }}
+            />
+            <Input
+              placeholder="Password"
+              leftIcon={{ type: "font-awesome", name: "lock" }}
+            />
+            <Button
+              icon={
+                <Icon
+                  name="arrow-right"
+                  color="#ffffff"
+                  iconStyle={{ marginRight: 8 }}
+                />
+              }
+              buttonStyle={{
+                borderRadius: 0,
+                marginLeft: 0,
+                marginRight: 0,
+                marginBottom: 0,
+              }}
+              title="Sign In"
+            />
+          </Card>
+        </View>
+      </ScrollView>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 128,
+  },
+  fonts: {
+    marginBottom: 8,
+  },
+  user: {
+    flexDirection: "row",
+    marginBottom: 6,
+  },
+  image: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+  },
+  name: {
+    fontSize: 16,
+    marginTop: 5,
   },
 });
+
+export default App;
